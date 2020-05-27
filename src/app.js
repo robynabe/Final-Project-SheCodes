b6f09a0f7762551176b96a9381a36d38e593dae3
robynabe <64428921+robynabe@users.noreply.github.com>
2020-05-27 14:25:17 -0700

    Implemented search engine using Open Weather

--- src/app.js
+++ src/app.js
@@ -32,11 +32,30 @@ let now = new Date();
 let currentDate = document.querySelector("#date-time");
 currentDate.innerHTML = formatDate(now);
 
+function displayCurrentWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#real-feel").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+  console.log(response);
+}
+
 function search(event) {
   event.preventDefault();
-  let findCity = document.querySelector("#city");
-  let cityInput = document.querySelector("#search-input");
-  findCity.innerHTML = cityInput.value;
+  let apiKey = "47aecf87524427e10968c44895784738";
+  let city = document.querySelector("#search-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCurrentWeather);
 }
+
 let searchForm = document.querySelector("#search-location");
 searchForm.addEventListener("submit", search);
