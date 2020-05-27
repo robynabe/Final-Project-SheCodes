281e6210b2729340203a4456fe9fa396dcc504af
robynabe <64428921+robynabe@users.noreply.github.com>
2020-05-27 12:01:32 -0700

    reformatted find current date function

--- src/app.js
+++ src/app.js
@@ -1,51 +1,41 @@
+function formatDate(date) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let dayDate = now.getDate();
+  let year = now.getFullYear();
+  let hour = now.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minute = now.getMinutes();
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${month} ${dayDate}, ${year} ${hour}:${minute}`;
+}
 let now = new Date();
-let p = document.querySelector("#date-time");
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let date = now.getDate();
-let year = now.getFullYear();
-let hour = now.getHours();
-let minute = now.getMinutes();
-
-p.innerHTML = `${day} ${month} ${date}, ${year} ${hour}:${minute}`;
-
-function searchCity(city) {
-  let apiKey = "47aecf87524427e10968c44895784738";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemp);
-}
-function displayTemp(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = response.data.main.temp;
-}
-function findWeather(event) {
-  event.preventDefault();
-  let city = document.querySelector("#search-input").value;
-  searchCity(city);
-}
-let searchForm = document.querySelector("#search-location");
-searchForm.addEventListener("submit", findWeather);
+let currentDate = document.querySelector("#date-time");
+currentDate.innerHTML = formatDate(now);
